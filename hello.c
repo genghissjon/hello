b9aa6f2c08cb01c6877853536de590ac7d427668
jonathan czepa <jonathan.czepa@gmail.com>
2022-08-25 20:59:38 +0000

    added hello world code

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *Author: Jonathan Czepa
+ *
+ *Date: 8/23/22
+ *
+ *A simple hello world style print statement
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+    printf("Hello world, my name is Jonathan!\n");
+
+    return 0;
+ }
